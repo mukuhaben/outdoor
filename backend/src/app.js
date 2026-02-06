0a10fbf530e685f858b7175b8169ca08ae81fbd1
mukuhaben <254engineerben@example.com>
2026-02-06 09:53:00 +0300

    #

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,6 +15,14 @@ app.use('/api/payments/webhook', paymentWebhookRoutes);//If express.json() runs 
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));//uploads static files.
+
+
+
+app.use("/api/bookings", bookingRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/admin", adminRoutes);
+app.use('/api/events', eventsRoutes)
+
 // Serve frontend folder
 app.use(express.static(path.join(__dirname, "../../frontend")));
 // Admin pages fallback
@@ -23,14 +31,5 @@ app.get(/^\/admin\/(?!api).*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../../frontend/admin/index.html"));
 });
 
-
-
-
-
-
-app.use("/api/bookings", bookingRoutes);
-app.use("/api/payments", paymentRoutes);
-app.use("/api/admin", adminRoutes);
-app.use('/api/events', eventsRoutes)
 
 module.exports = app;
