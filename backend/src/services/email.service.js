e7e1a373ce017533034f7f4d974d4edd41a785c6
mukuhaben <254engineerben@example.com>
2026-02-06 08:28:11 +0300

    added tls in email service.js  to zoho for render

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -7,6 +7,10 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
+  },
+    tls: {
+    servername: process.env.SMTP_HOST, // 🔑 important for Zoho
+    rejectUnauthorized: false          // 🔑 Render cert issue
   }
 });
 
